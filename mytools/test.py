da062916ab683a33f2028871fda1f986878f7a62
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2021-03-11 00:43:47 +0900

    inとoutの指定

--- mytools/test.py
+++ mytools/test.py
@@ -8,12 +8,20 @@ def runShell(s):
 
 # cppFile = input()
 print(sys.argv)
-cppFile = sys.argv[1]
+cppFile = "src/a.cpp"
+inFile = "./tools/example.in"
+outFile = "out.txt"
+
+if len(sys.argv) >= 2:
+    cppFile = sys.argv[1]
+if len(sys.argv) >= 3:
+    inFile = sys.argv[2]
+if len(sys.argv) >= 4:
+    outFile = sys.argv[3]
+
 compile = "g++-9 -O2 -std=gnu++17 -Wall -Wextra -Wshadow -DDEBUG {}".format(cppFile)
 
 print(runShell("pwd"))
-inFile = "./tools/example.in"
-outFile = "out.txt"
 
 jikkou = "./a.out < {} > {}".format(inFile, outFile)
 
